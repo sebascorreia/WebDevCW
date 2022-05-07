1ff6db54e3e114a4723f873572eed20098a57c1c
fc00389 <fc00389@surrey.ac.uk>
2022-05-07 19:41:59 +0100

    copy from master to local branch

---
+++ hospital/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+app_name = "hospital"
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register', views.patient_registration, name='register'),
+    path('login', views.login_request, name='login'),
+]
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1> DJANGO SETUP: </h1>
+python -m django --version
+<br> python manage.py runserver
