de5fdbc211971a10897253460ae08a6819ad2496
sc01879 <sc01879@surrey.ac.uk>
2022-05-09 11:16:39 +0100

    Ashwins code + appointments route

--- hospital/models.py
+++ hospital/models.py
@@ -48,7 +48,7 @@ class Patient(models.Model):
 
 class Appointment(models.Model):
     patientId = models.ForeignKey(Patient, on_delete= models.CASCADE)
-    doctorId = models.ForeignKey(Doctor, on_delete= models.CASCADE)
+    doctorId = models.ForeignKey(Doctor,null= True, on_delete= models.CASCADE)
     appointmentDate = models.DateField(null = True)
     description = models.TextField(max_length=1000)
     status = models.BooleanField(default=False)
